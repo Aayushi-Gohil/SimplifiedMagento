c1b463b52f2442c16ef5b696ceecdad6414facae
Aayushi <aayushi.gohil@krishtechnolabs.com>
2021-04-19 17:49:54 +0530

    [TASK] Adding More Example

---
+++ magento/app/code/SimplifiedMagento/CatalogWidget/Model/Config/Source/SortBy.php
@@ -0,0 +1,18 @@
+<?php
+namespace SimplifiedMagento\CatalogWidget\Model\Config\Source;
+
+class SortBy implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'name', 'label' => __('Product Name')],
+            ['value' => 'price', 'label' => __('Price')]
+        ];
+    }
+}
